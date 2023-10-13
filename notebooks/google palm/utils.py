eb0ed0ba91692be87c8bf4490b1baeaa606d7c98
kolommik <mikhail.kolomasov@gmail.com>
2023-10-13 11:15:55 +0300

    update docstrings in utils.py

--- notebooks/google palm/utils.py
+++ notebooks/google palm/utils.py
@@ -1,7 +1,16 @@
+"""Utility functions.
+
+This module contains utility functions for the palm-models package.
+"""
 import os
 from dotenv import load_dotenv, find_dotenv
 
+
 def get_api_key():
-    
-    _ = load_dotenv(find_dotenv()) # read local .env file
-    return os.getenv('GOOGLE_API_KEY')
+    """Get the Google API key from the local .env file.
+
+    Returns:
+        str: The Google API key.
+    """
+    _ = load_dotenv(find_dotenv())  # read local .env file
+    return os.getenv("GOOGLE_API_KEY")
